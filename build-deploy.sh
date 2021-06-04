d02748eebf5b5ac71fdace48ad1185d69fc86a2e
rammohanc <rchitimilla@vmware.com>
2021-06-03 23:31:16 -0500

    build and deploy spring-petclinic v0.5

---
+++ build-deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -o errexit
+
+export APP_NAME="${1:-spring-petclinic}"
+export APP_IMAGE_REVISION="${2:-v1.0}"
+export APP_GIT_URL="${3:-https://github.com/rammohanc/spring-petclinic.git}"
+export APP_IMAGE_TAG="${4:-harbor.run.haas-222.pez.pivotal.io/tbs/spring-petclinic}"
+export APP_GITOPS_URL="${5:-https://github.com/rammohanc/gitops.git}"
+export APP_IMAGE_REVISION_NO_PERIOD="$(echo "${APP_IMAGE_REVISION//./}")" 
+
+ytt -f templates/build --data-value-yaml metadata_name=${APP_NAME}-${APP_IMAGE_REVISION} --data-value-yaml git_revision=$APP_IMAGE_REVISION --data-value-yaml git_url=$APP_GIT_URL --data-value-yaml tag=${APP_IMAGE_TAG}:${APP_IMAGE_REVISION} -o yaml > tbs/spring-petclinic-image.yaml
+
+
+ytt -f templates/deploy --data-value-yaml ksvc_metadata_name=${APP_NAME}-${APP_IMAGE_REVISION_NO_PERIOD} --data-value-yaml tag=${APP_IMAGE_TAG}:${APP_IMAGE_REVISION} -o yaml > ksvc/spring-petclinic-serverless.yaml
+
+
+git add .
+git commit -m "build and deploy $APP_NAME $APP_IMAGE_REVISION"
+git tag $APP_IMAGE_REVISION
+git push --tags
+
+argocd app create spring-petclinic-tbs-image --sync-policy auto --repo $APP_GITOPS_URL --revision $APP_IMAGE_REVISION  --path tbs --dest-server https://kubernetes.default.svc --dest-namespace tbs
+
+
+sleep 300
+#TEST_READINESS=`kubectl get images.kpack.io ${APP_NAME}-${APP_IMAGE_REVISION} -o json | jq -r '.status.conditions[] | select(.type=="Ready") | .status'`
+#while [ ! $TEST_READINESS = "True" ]; do
+#    echo $(kubectl get images.kpack.io ${APP_NAME}-${APP_IMAGE_REVISION} -o json | jq -r '.status.conditions[] | select(.type=="Ready") | .message')
+#    sleep 60
+#    TEST_READINESS=`kubectl get images.kpack.io ${APP_NAME}-${APP_IMAGE_REVISION} -o json | jq -r '.status.conditions[] | select(.type=="Ready") | .status'`
+#done
+
+echo "Image built.."
+echo "Deploying image .."
+
+argocd app create spring-petclinic-serverless --sync-policy auto --repo $APP_GITOPS_URL --revision $APP_IMAGE_REVISION  --path ksvc --dest-server https://kubernetes.default.svc --dest-namespace tbs
+
+
+
+#ytt -f deploy.yaml --data-value-yaml APP_NAME=$APP_NAME --data-value-yaml APP_IMAGE_DIGEST="$(kubectl get images.kpack.io $APP_NAME -o json | jq -r '.status.latestImage')" -o yaml > app-deploy/deploy.yaml
+#
+#git add .
+#git commit -m "update $APP_NAME $APP_IMAGE_REVISION"
+#git push
