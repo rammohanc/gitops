d8753799bfcbbc592ec9978c2a58ada223fc09ee
rammohanc <rchitimilla@vmware.com>
2021-06-11 10:06:11 -0500

    build and deploy spring-petclinic v1.14

--- build-deploy.sh
+++ build-deploy.sh
@@ -3,10 +3,22 @@ set -o errexit
 
 export APP_NAME="${1:-spring-petclinic}"
 export APP_IMAGE_REVISION="${2:-v1.0}"
-export APP_GIT_URL="${3:-https://github.com/rammohanc/spring-petclinic.git}"
-export APP_IMAGE_TAG="${4:-harbor.run.haas-222.pez.pivotal.io/tbs/spring-petclinic}"
-export APP_GITOPS_URL="${5:-https://github.com/rammohanc/gitops.git}"
+export COMMIT_MESSAGE="${3:-$(echo "updated message")}"
+export APP_GIT_URL="${4:-https://github.com/rammohanc/spring-petclinic.git}"
+export APP_IMAGE_TAG="${5:-harbor.run.haas-222.pez.pivotal.io/tbs/spring-petclinic}"
+export APP_GITOPS_URL="${6:-https://github.com/rammohanc/gitops.git}"
 export APP_IMAGE_REVISION_NO_PERIOD="$(echo "${APP_IMAGE_REVISION//./}")" 
+
+
+#for demo only
+cd /Users/rammohanchitimilla/ocp/spring-petclinic
+git add . && git commit -m "$COMMIT_MESSAGE"
+git tag $APP_IMAGE_REVISION
+git push origin --tags
+cd /Users/rammohanchitimilla/ocp/gitops
+echo "spring-petclinic app version $APP_IMAGE_REVISION pushed to git"
+
+
 
 ytt -f templates/build --data-value-yaml metadata_name=${APP_NAME} --data-value-yaml git_revision=$APP_IMAGE_REVISION --data-value-yaml git_url=$APP_GIT_URL --data-value-yaml tag=${APP_IMAGE_TAG} -o yaml > tbs/spring-petclinic-image.yaml
 
@@ -30,7 +42,7 @@ while [ ! $TEST_READINESS = "True" ]; do
     TEST_READINESS=`kubectl get images.kpack.io ${APP_NAME} -o json | jq -r '.status.conditions[] | select(.type=="Ready") | .status'`
 done
 
-echo "Image built.."
+echo "Image built by Tanzu Build Service.."
 echo "Deploying image .."
 
 #argocd app create spring-petclinic-serverless --upsert --sync-policy auto --repo $APP_GITOPS_URL --revision $APP_IMAGE_REVISION  --path ksvc --dest-server https://kubernetes.default.svc --dest-namespace tbs
@@ -44,6 +56,8 @@ ytt -f templates/deploy --data-value-yaml ksvc_metadata_name=${APP_NAME} --data-
 git add .
 git commit -m "update $APP_NAME $APP_IMAGE_REVISION"
 git push
+
+echo "Deployed image .."
 
 
 #argocd app create spring-petclinic-serverless --upsert --sync-policy auto --repo $APP_GITOPS_URL  --revision main --path ksvc --dest-server https://kubernetes.default.svc --dest-namespace tbs
